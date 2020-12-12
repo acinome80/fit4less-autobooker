39812ac26fcafc6aba04ed3ee87b0fa5b26addcc
krisan <km.krisan@gmail.com>
2020-12-12 01:29:21 -0500

    Update autobooker.py

--- autobooker.py
+++ autobooker.py
@@ -70,7 +70,7 @@ try:
         try:            
             reserved_slots = driver.find_element_by_class_name("reserved-slots").find_elements_by_class_name("time-slot-box")      
             for slot in reserved_slots:                
-                if str(curr_dt.day + i) == str(slot.text).split()[3]:
+                if str(curr_dt.day + i) == str(slot.text).split()[4]:
                     print("Already have reservation for the day: ", booking_date)
                     already_have_reservation = True
                     break
