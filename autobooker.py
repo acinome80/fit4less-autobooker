cc104e5128cd4914c5381e645f829405b1af7899
krisan <km.krisan@gmail.com>
2020-12-11 20:03:47 -0500

    check reserved date

--- autobooker.py
+++ autobooker.py
@@ -64,10 +64,7 @@ try:
         reserved_slots = driver.find_element_by_class_name("reserved-slots").find_elements_by_class_name("time-slot-box")
         already_have_reservation = False
         for slot in reserved_slots:
-            print("SLOT TEXT: ", str(slot.text))
-            a_slot = datetime.strptime(str(slot.text).split()[5] + str(slot.text).split()[6], '%I:%M%p')
-            reserved_day = a_slot.weekday()
-            if curr_day == reserved_day:
+            if str(datetime.now(timezone('est')).day) == str(slot.text).split()[4]:
                 print("Already have reservation for the day: ", booking_date)
                 already_have_reservation = True
                 break
